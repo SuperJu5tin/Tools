8e914e09f25399cf7a8ecb861940607199d2d02a
SuperJu5tin <superjuslib@gmail.com>
2023-10-26 10:56:55 -0600

    got the variable getter done

--- Python/Physics/Knowns To Unknowns/Kinematics.py
+++ Python/Physics/Knowns To Unknowns/Kinematics.py
@@ -6,25 +6,60 @@ x = x₀ + vₓ₀t + (aₓ * t²) / 2
 vₓ² = vₓ₀² + 2aₓ(x - x₀)
 """
 
+# class unkownGetter:
+#   def __int__(self, v_x, v_x_initial, x, a, t):
+#     self.v_x = v_x
+#     self.v_x_initial = v_x_initial
+#     self.x = x
+#     self.a = a
+#     self.t = t
+#
+#   known = []
+#
+#   def
+
+knowns = []
+unknowns = []
+
+
+def info_getter(name_of_var):
+  user_input = input("\tEnter " + name_of_var + ":")
+
+  try:
+    float(user_input)
+    knowns.append(name_of_var)
+    return user_input
+  except ValueError:
+    if user_input == "":
+      unknowns.append(name_of_var)
+    else:
+      print("Try again")
+      info_getter(name_of_var)
+
 
 def main():
   done = False
 
   while not done:
+    print("\nEnter Knowns:\n")
 
-    print("Enter Knowns:")
-    print("\t . None")
+    knowns.clear()
+    unknowns.clear()
 
-    v_x = "test"
-    v_x_initial = "test2"
-    x = "test3"
-    a = "test4"
-    t = "test5"
+    v_x = info_getter("v_x")
+    v_x_initial = info_getter("v_x_initial")
+    x = info_getter("x")
+    a = info_getter("a")
+    t = info_getter("t")
 
-    placeholder = input("Enter equation: ").lower()
+    # print(v_x, v_x_initial, x, a, t, '\n', knowns, unknowns)
+
+    if len(knowns) >= 3:
+      print("this is solvable")
+    else:
+      print("Try again, you need at least 3 variables to solve")
 
     # Possible class/object and for each to figure out which ones exist and which don't, {variable: { known: Boolean, value: int }, variable2: { known: Boolean, value: int }, etc}
-    # detect blank "" for unknown
 
     # if equation_type == '1':
     #   velocity_x()
